a156667a93e2c04ebc21f93e4d7090a39baf9251
emilie-lb <emilielb75@gmail.com>
2021-05-15 15:04:09 +0200

    ajout d'un test

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,12 +12,12 @@ class Testbasics(unittest.TestCase):
         result = self.app.get("/")
         self.assertEqual(result.status_code, 200)
 
-    # def test_hello_world(self):
-    #     ''' sends HTTP GET request to the application
-    #         on the specified path '''
-    #     self.app = app.test_client()
-    #     result = self.app.get("/hello_world")
-    #     self.assertEqual(result.status_code, 200)
+    def test_hello_world(self):
+        ''' sends HTTP GET request to the application
+            on the specified path '''
+        self.app = app.test_client()
+        result = self.app.get("/hello_world")
+        self.assertEqual(result.status_code, 200)
 
 
 if __name__ == '__main__':
